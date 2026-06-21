3bf685c9cded1a5708d06b668724d37008b58adc
ceciliacole <ceciliamedina@colegiogenil.com>
2026-06-21 23:50:18 +0200

    Update backend URL and access key in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Pega aquí la URL /exec que obtendrás al publicar el backend de Apps Script.
 window.APP_CONFIG = {
-  backendUrl: "PEGA_AQUI_LA_URL_DE_APPS_SCRIPT_TERMINADA_EN_EXEC",
-  accessKey: "CAMBIA_ESTA_CLAVE_EN_WEB_Y_BACKEND"
+  backendUrl: "https://script.google.com/macros/s/AKfycbyAyww7o0ZxinAtVCZm57hXdZriLwn3kJ2WT3nhoj3tjVWUrQKk1r6h3nHKFBA6WIg1/exec",
+  accessKey: "lectura_6B"
 };
