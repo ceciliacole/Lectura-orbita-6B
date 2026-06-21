a1cfa052f5210710b1df1d5897fd1df1b09f1a59
ceciliacole <ceciliamedina@colegiogenil.com>
2026-06-21 23:49:03 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Pega aquí la URL /exec que obtendrás al publicar el backend de Apps Script.
-window.APP_CONFIG = {
-  backendUrl: "https://script.google.com/macros/s/AKfycbyAyww7o0ZxinAtVCZm57hXdZriLwn3kJ2WT3nhoj3tjVWUrQKk1r6h3nHKFBA6WIg1/exec",
-  accessKey: "lectura_6B"
-};
