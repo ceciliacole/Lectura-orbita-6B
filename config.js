e3e2c25991ee79dcb9773f3aee927bc66034e1b2
ceciliacole <ceciliamedina@colegiogenil.com>
2026-06-21 23:11:39 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Pega aquí la URL /exec que obtendrás al publicar el backend de Apps Script.
+window.APP_CONFIG = {
+  backendUrl: "https://script.google.com/macros/s/AKfycbyHAYpehC--WSz0WzdaSPirwnBj4UsAa4M03cLUGXL74ysUk3ELU_z1DIzot2VxIvvA/exec",
+  accessKey: "lectura_6B"
+};
